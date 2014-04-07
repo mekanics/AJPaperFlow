2223457b33854c049b8f4059e698f9664fc22fe8
Alexandre Joly <alexandre.joly@mekanics.ch>
2014-04-07 20:42:51 +0200

    Initial Commit

---
+++ AJSeeEveryViews/AJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AJAppDelegate.h
+//  AJSeeEveryViews
+//
+//  Created by Alexandre Joly on 07/04/14.
+//  Copyright (c) 2014 Alexandre Joly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AJSeeEveryViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AJSeeEveryViews
+//
+//  Created by Alexandre Joly on 07/04/14.
+//  Copyright (c) 2014 Alexandre Joly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AJAppDelegate class]));
+    }
+}
